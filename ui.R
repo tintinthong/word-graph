8b91433b8a9d30f8a63664ebf619fd879e0fe9a1
Justin <justinthong93@gmail.com>
2019-08-28 01:37:44 +0800

    centered all elements

--- ui.R
+++ ui.R
@@ -5,6 +5,12 @@ ui<-fluidPage(
   # using shiny js
   useShinyjs(),
   
+  fluidRow(align="center",
+    column(4, 
+           titlePanel('Word Context Matching')
+           ,offset=4)
+  ),
+  
   # loop through ui
   fluidRow(align="center",
     lapply(itemsList, function(item) {
@@ -17,14 +23,15 @@ ui<-fluidPage(
   
   
   # make this render UI maybe
-  fluidRow(
-    column(4,uiOutput("slider"),offset=4
-           ),
-    column(2,actionButton("submit", "Submit"), offset=8),
-    column(2, actionButton("finish","Finish"),offset=8)
+  fluidRow(align="center",
+    column(4,uiOutput("slider"),offset=4)
   ),
-  fluidRow(
-    column(2,htmlOutput("nextWord"),offset=4)
+  fluidRow(align="center",
+           column(4,htmlOutput("nextWord"),offset=4)
+  ),
+  fluidRow(align="center",
+           column(4,actionButton("submit", "Submit"),offset=4),
+           column(4, actionButton("finish","Finish"),offset=4)
   )
 
   
